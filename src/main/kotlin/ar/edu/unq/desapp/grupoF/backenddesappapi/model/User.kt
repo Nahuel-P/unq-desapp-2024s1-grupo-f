4c2d8129cfb6904a7c2e7964449dba840f6494a6
unknown <julivazquez09@gmail.com>
2024-04-13 15:13:01 -0300

    fix: something

--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/model/User.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/model/User.kt
@@ -8,7 +8,6 @@ import jakarta.persistence.Id
 import java.util.regex.Pattern
 
 class User{
-
     var id: Long? = null
     var firstName: String? = null
     var lastName: String? = null
@@ -17,22 +16,4 @@ class User{
     var password: String? = null
     var cvu: String? = null
     var walletAddress: String? = null
-
-    private fun validateLength(value: String, min: Int, max: Int, fieldName: String): String {
-        require(value.length in min..max) { "$fieldName must be between $min and $max characters" }
-        return value
-    }
-
-    private fun validateEmail(value: String): String {
-        require(Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$").matcher(value).matches()) { "Invalid email format" }
-        return value
-    }
-
-    private fun validatePassword(value: String): String {
-        require(Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{6,}$").matcher(value).matches()) { "Password must contain at least 1 lowercase letter, 1 uppercase letter, 1 special character and be at least 6 characters long" }
-        return value
-    }
-    fun publishOrder(intention: IntentionType, cryptocurrency: Cryptocurrency, amount: Double, price: Double): Order {
-        return Order(this, cryptocurrency, amount, price, intention)
-    }
 }
