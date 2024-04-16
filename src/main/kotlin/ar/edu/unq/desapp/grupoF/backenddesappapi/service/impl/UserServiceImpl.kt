2e62affc70d011a67bf0c52f567cde76cbe83b8a
unknown <julivazquez09@gmail.com>
2024-04-16 14:46:56 -0300

    feature: user registration service

--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/service/impl/UserServiceImpl.kt
@@ -1,6 +1,7 @@
 package ar.edu.unq.desapp.grupoF.backenddesappapi.service
 
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.User
+import ar.edu.unq.desapp.grupoF.backenddesappapi.model.builder.UserBuilder
 import ar.edu.unq.desapp.grupoF.backenddesappapi.webservice.dto.UserCreateRequestDTO
 import org.springframework.stereotype.Service
 
@@ -8,8 +9,15 @@ import org.springframework.stereotype.Service
 class UserServiceImpl : UserService {
     override fun registerUser(userCreateRequest: UserCreateRequestDTO): User {
         require(!(userCreateRequest.email == null || userCreateRequest.email!!.isEmpty())) { "Email must not be null or empty" }
-        val user = User()
-        user.email = userCreateRequest.email
+        val user = UserBuilder()
+            .withFirstName(userCreateRequest.firstName!!)
+            .withLastName(userCreateRequest.lastName!!)
+            .withEmail(userCreateRequest.email!!)
+            .withAddress(userCreateRequest.address!!)
+            .withPassword(userCreateRequest.password!!)
+            .withCvu(userCreateRequest.cvu!!)
+            .withWalletAddress(userCreateRequest.walletAddress!!)
+            .build()
         return user
     }
 }
