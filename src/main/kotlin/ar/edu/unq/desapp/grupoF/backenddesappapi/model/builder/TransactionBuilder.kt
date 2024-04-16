907e36294d404ce2a3d36f7f21231e8b2ffbddf1
Nahuel-P <nahuel.pacheco@protonmail.com>
2024-04-16 02:54:17 -0300

    style:  deleted commented lines

--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/model/builder/TransactionBuilder.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/model/builder/TransactionBuilder.kt
@@ -3,20 +3,15 @@ package ar.edu.unq.desapp.grupoF.backenddesappapi.model.builder
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.Order
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.Transaction
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.User
-import ar.edu.unq.desapp.grupoF.backenddesappapi.model.enums.TransactionStatus
-import java.time.LocalDateTime
 
 class TransactionBuilder {
 //    private var id: Long? = null
     private var order: Order? = null
     private var counterParty: User? = null
-//    private var status: TransactionStatus? = TransactionStatus.PENDING
-//    private var entryTime: LocalDateTime = LocalDateTime.now()
-//    private var endTime: LocalDateTime? = null
-//    private var isActive: Boolean = false
+
 
     fun build(): Transaction {
-        var transaction = Transaction()
+        val transaction = Transaction()
         transaction.order = this.order
         transaction.counterParty = this.counterParty
         return transaction
@@ -31,30 +26,5 @@ class TransactionBuilder {
         this.order = order
         return this
     }
-
-//    fun withStatus(status: TransactionStatus): TransactionBuilder {
-//        this.status = status
-//        return this
-//    }
-//
-//    fun withEntryTime(entryTime: LocalDateTime): TransactionBuilder {
-//        this.entryTime = entryTime
-//        return this
-//    }
-//
-//    fun withEndTime(endTime: LocalDateTime): TransactionBuilder {
-//        this.endTime = endTime
-//        return this
-//    }
-//
-//    fun withIsActive(isActive: Boolean): TransactionBuilder {
-//        this.isActive = isActive
-//        return this
-//    }
-//
-//    fun withId(id: Long): TransactionBuilder {
-//        this.id = id
-//        return this
-//    }
 
 }
