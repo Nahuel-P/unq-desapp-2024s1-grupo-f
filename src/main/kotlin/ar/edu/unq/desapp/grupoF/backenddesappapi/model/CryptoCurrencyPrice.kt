9f3a3d99928c3796bff48e260962040fefe7c263
Nahuel-P <nahuel.pacheco@protonmail.com>
2024-05-12 23:21:08 -0300

    refactor: changed mapping to dto

--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/model/CryptoCurrencyPrice.kt
+++
@@ -1,6 +0,0 @@
-package ar.edu.unq.desapp.grupoF.backenddesappapi.model
-
-data class CryptoCurrencyPrice(
-    val name: String,
-    val price: Double
-)
--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/webservice/CryptoCurrencyController.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/webservice/CryptoCurrencyController.kt
@@ -2,6 +2,7 @@ package ar.edu.unq.desapp.grupoF.backenddesappapi.webservice
 
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.enums.CryptoSymbol
 import ar.edu.unq.desapp.grupoF.backenddesappapi.service.client.BinanceClient
+import ar.edu.unq.desapp.grupoF.backenddesappapi.service.dto.CryptocurrencyPriceDTO
 import io.swagger.v3.oas.annotations.tags.Tag
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
@@ -17,11 +18,11 @@ class CryptoCurrencyController {
     private val binanceClient = BinanceClient()
 
     @GetMapping("/prices")
-    fun getPrices(): Map<String, Double> = runBlocking {
+    fun getPrices(): List<CryptocurrencyPriceDTO> = runBlocking {
         CryptoSymbol.entries.map { symbol ->
             async {
-                symbol.toString() to binanceClient.getCryptoCurrencyPrice(symbol).price!!
+                CryptocurrencyPriceDTO(symbol.toString(), binanceClient.getCryptoCurrencyPrice(symbol).price!!)
             }
-        }.associate { it.await() }
+        }.map { it.await() }
     }
 }
