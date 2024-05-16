34710d844410e98af3ea0f57cf640627ab2a0f98
unknown <julivazquez09@gmail.com>
2024-05-15 22:40:13 -0300

    feat: more about transaction end-to-end

--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/service/impl/TransactionServiceImpl.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/service/impl/TransactionServiceImpl.kt
@@ -3,16 +3,20 @@ package ar.edu.unq.desapp.grupoF.backenddesappapi.service.impl
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.Transaction
 import ar.edu.unq.desapp.grupoF.backenddesappapi.repositories.TransactionRepository
 import ar.edu.unq.desapp.grupoF.backenddesappapi.service.ITransactionService
+import ar.edu.unq.desapp.grupoF.backenddesappapi.service.IUserService
 import org.springframework.stereotype.Service
 
 @Service
-class TransactionServiceImpl(private val transactionRepository: TransactionRepository) : ITransactionService {
+class TransactionServiceImpl(
+    private val transactionRepository: TransactionRepository,
+    private val userService: IUserService) : ITransactionService {
 
     override fun create(transaction: Transaction): Transaction {
 ////        recoverTrade(transaction)
 //        transaction.order = orderService.getOrderById(transaction.order.id)
 //        val userRequested = recoverUserRequested(transaction)
 //        updateBuyerSeller(transaction, userRequested)
+        transaction.counterParty = userService.findUser(transaction.counterParty?.id!!)
         return transactionRepository.save(transaction)
 //        return transaction
     }
