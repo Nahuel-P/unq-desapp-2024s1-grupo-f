4ba4325723b9571274f32b4cad489435a2acb479
Nahuel-P <nahuel.pacheco@protonmail.com>
2024-05-18 20:53:38 -0300

    tests: fix for profile

--- src/test/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/service/client/BinanceClientTest.kt
+++ src/test/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/service/client/BinanceClientTest.kt
@@ -2,9 +2,23 @@ package ar.edu.unq.desapp.grupoF.backenddesappapi.service.client
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.enums.CryptoSymbol
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.springframework.test.context.ActiveProfiles
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.core.env.MapPropertySource
+import org.springframework.test.context.ContextConfiguration
 
-@ActiveProfiles("dev")
+class ActiveProfileResolver : ApplicationContextInitializer<ConfigurableApplicationContext> {
+    override fun initialize(applicationContext: ConfigurableApplicationContext) {
+        val profiles = System.getenv("SPRING_PROFILES_ACTIVE") ?: "dev"
+        val map: Map<String, Any> = mapOf("spring.profiles.active" to profiles)
+        val propertySource = MapPropertySource("myPropertySource", map)
+        applicationContext.environment.propertySources.addFirst(propertySource)
+    }
+}
+
+@SpringBootTest
+@ContextConfiguration(initializers = [ActiveProfileResolver::class])
 class BinanceClientTest {
 
     @Test
