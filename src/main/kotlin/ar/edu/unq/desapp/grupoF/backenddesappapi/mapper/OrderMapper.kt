e3ab0dd24a66172bb2ac6c01f4b4bfb4273db620
Nahuel-P <nahuel.pacheco@protonmail.com>
2024-05-19 00:00:00 -0300

    refactor: mapper

--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/mapper/OrderMapper.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/mapper/OrderMapper.kt
@@ -1,21 +1,16 @@
 package ar.edu.unq.desapp.grupoF.backenddesappapi.mapper
 
+import ar.edu.unq.desapp.grupoF.backenddesappapi.model.Cryptocurrency
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.Order
+import ar.edu.unq.desapp.grupoF.backenddesappapi.model.User
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.builder.OrderBuilder
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.enums.IntentionType
-import ar.edu.unq.desapp.grupoF.backenddesappapi.service.ICryptoService
-import ar.edu.unq.desapp.grupoF.backenddesappapi.service.IUserService
 import ar.edu.unq.desapp.grupoF.backenddesappapi.webservice.dto.OrderRequestDTO
-import java.util.*
 
 class OrderMapper {
 
     companion object {
-        fun fromCreateDto(userService: IUserService, cryptoService: ICryptoService, dto: OrderRequestDTO): Order {
-            val user = userService.getUser(dto.userId)
-            val intentionType = IntentionType.valueOf(dto.type.uppercase(Locale.getDefault()))
-
-            val crypto = cryptoService.getCrypto(dto.cryptocurrency)
+        fun fromCreateDto(dto: OrderRequestDTO, user: User, intentionType: IntentionType, crypto: Cryptocurrency): Order {
             return OrderBuilder()
                 .withOwnerUser(user)
                 .withCryptocurrency(crypto)
--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/webservice/OrderController.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/webservice/OrderController.kt
@@ -1,31 +1,44 @@
 package ar.edu.unq.desapp.grupoF.backenddesappapi.webservice
 
 import ar.edu.unq.desapp.grupoF.backenddesappapi.mapper.OrderMapper
+import ar.edu.unq.desapp.grupoF.backenddesappapi.model.enums.IntentionType
 import ar.edu.unq.desapp.grupoF.backenddesappapi.service.ICryptoService
 import ar.edu.unq.desapp.grupoF.backenddesappapi.service.IOrderService
 import ar.edu.unq.desapp.grupoF.backenddesappapi.service.IUserService
 import ar.edu.unq.desapp.grupoF.backenddesappapi.webservice.dto.OrderRequestDTO
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/order")
 @Tag(name = "Order", description = "Endpoints for order operations")
 class OrderController(
-    private val orderService: IOrderService,
-    private val userService: IUserService,
-    private val cryptoService: ICryptoService
+
 
 ) {
+    @Autowired
+    private lateinit var orderService: IOrderService
+
+    @Autowired
+    private lateinit var userService: IUserService
+    @Autowired
+    private lateinit var cryptoService: ICryptoService
 
     @Operation (summary = "Create a new order")
     @PostMapping("/createOrder")
-    fun createOrder(@RequestBody request: OrderRequestDTO): ResponseEntity<Any> {
+    fun createOrder(@RequestBody dto: OrderRequestDTO): ResponseEntity<Any> {
         return try {
-            val order = OrderMapper.fromCreateDto(userService,cryptoService, request)
+            val user = userService.getUser(dto.userId)
+            val intentionType = IntentionType.valueOf(dto.type.uppercase(Locale.getDefault()))
+            val crypto = cryptoService.getCrypto(dto.cryptocurrency)
+
+            val order = OrderMapper.fromCreateDto(dto, user, intentionType, crypto)
+
             orderService.createOrder(order)
             ResponseEntity.status(HttpStatus.OK).body(order)
         } catch (e: Exception) {
