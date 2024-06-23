60abd0f6f77def259cce18d9fbebc9fedc9c7257
unknown <julivazquez09@gmail.com>
2024-06-23 18:42:05 -0300

    feat: add actives for report

--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/model/UserVolumeReport.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/model/UserVolumeReport.kt
@@ -4,6 +4,20 @@ import java.time.LocalDateTime
 class UserVolumeReport{
     var totalUSD: Double = 0.0
     var totalARG: Double = 0.0
-    var actives: MutableList<Active> = mutableListOf()
+    var actives: MutableMap<String, Active> = mutableMapOf()
     val requestDate: LocalDateTime = LocalDateTime.now()
+
+    fun addToVolumeReport(transaction: Transaction) {
+        totalUSD += transaction.usdPrice() * transaction.nominalAmount()
+        totalARG += transaction.arsQuote()
+
+        val activeName = transaction.order!!.cryptocurrency!!.name.toString()
+        val active = actives.getOrDefault(activeName, Active())
+        active.crypto = transaction.order!!.cryptocurrency!!.name
+        active.cryptoNominalQuantity += transaction.nominalAmount()
+        active.usdPrice += transaction.usdPrice()
+        active.argPrice += transaction.arsQuote()
+
+        actives[activeName] = active
+    }
 }
