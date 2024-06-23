a8e8a91ba3a3afa7d5b15ffe5badab095b85d904
Nahuel-P <nahuel.pacheco@protonmail.com>
2024-06-23 00:24:11 -0300

    test: architecture tests

---
+++ src/test/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/ArchitectureTest.kt
@@ -0,0 +1,30 @@
+import com.tngtech.archunit.core.domain.JavaClasses
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
+import org.junit.jupiter.api.Test
+import org.springframework.stereotype.Repository
+
+class ArchitectureTest {
+
+    @Test
+    fun `project should not use singletons`() {
+        val importedClasses: JavaClasses = ClassFileImporter().importPackages("ar.edu.unq.desapp.grupoF.backenddesappapi")
+
+        val rule: ArchRule = noClasses()
+            .should().beAnnotatedWith("javax.inject.Singleton")
+
+        rule.check(importedClasses)
+    }
+    @Test
+    fun `every file that ends with Repository should have the Repository annotation`() {
+        val importedClasses: JavaClasses = ClassFileImporter().importPackages("ar.edu.unq.desapp.grupoF.backenddesappapi")
+
+        val rule: ArchRule = classes()
+            .that().haveSimpleNameEndingWith("Repository")
+            .should().beAnnotatedWith(Repository::class.java)
+
+        rule.check(importedClasses)
+    }
+}
