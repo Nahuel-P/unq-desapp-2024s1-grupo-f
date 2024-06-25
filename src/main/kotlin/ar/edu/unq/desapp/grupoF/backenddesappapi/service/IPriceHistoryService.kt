b4e27ac3d69c22b073bf81c5b7b5fb392eda7406
Nahuel-P <nahuel.pacheco@protonmail.com>
2024-06-25 06:49:11 -0300

    fix: sonarcloud issues

--- src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/service/IPriceHistoryService.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/service/IPriceHistoryService.kt
@@ -3,7 +3,7 @@ package ar.edu.unq.desapp.grupoF.backenddesappapi.service
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.PriceHistory
 import ar.edu.unq.desapp.grupoF.backenddesappapi.model.enums.CryptoSymbol
 
-interface IPriceHistoryService {
+fun interface IPriceHistoryService {
 
     fun getLast24hsQuotes(symbol: CryptoSymbol): List<PriceHistory>
 }
