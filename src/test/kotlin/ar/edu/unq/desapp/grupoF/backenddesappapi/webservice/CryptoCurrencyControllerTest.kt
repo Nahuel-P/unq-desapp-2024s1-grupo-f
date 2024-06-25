80c9ed378ab243f1fc889d2bd9486fb9deebdc1d
Nahuel-P <nahuel.pacheco@protonmail.com>
2024-06-25 05:16:46 -0300

    test: cryptocurrencycontrollerTest

--- src/test/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/webservice/CryptoCurrencyControllerTest.kt
+++ src/test/kotlin/ar/edu/unq/desapp/grupoF/backenddesappapi/webservice/CryptoCurrencyControllerTest.kt
@@ -1,32 +1,44 @@
 package ar.edu.unq.desapp.grupoF.backenddesappapi.webservice
 
+import ar.edu.unq.desapp.grupoF.backenddesappapi.model.builder.CryptocurrencyBuilder
+import ar.edu.unq.desapp.grupoF.backenddesappapi.model.enums.CryptoSymbol
+import ar.edu.unq.desapp.grupoF.backenddesappapi.repositories.CryptocurrencyRepository
 import ar.edu.unq.desapp.grupoF.backenddesappapi.service.ICryptoService
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@EnabledIfEnvironmentVariable(named = "SPRING_PROFILES_ACTIVE", matches = "dev")
+@WithMockUser(username = "michael", roles = ["USER"])
 class CryptoCurrencyControllerTest {
-
-    @Autowired
-    private lateinit var mockMvc: MockMvc
 
     @MockBean
     private lateinit var cryptoService: ICryptoService
 
+    @MockBean
+    private lateinit var cryptocurrencyRepository: CryptocurrencyRepository
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
     @Test
-    fun `getPrices returns OK status`() {
-        Mockito.`when`(cryptoService.getQuotes()).thenReturn(listOf())
+    fun `getQuotes returns OK status when service returns data`() {
+        val cryptocurrency1 = CryptocurrencyBuilder().withName(CryptoSymbol.BTCUSDT).withPrice(50000.0).build()
+        val cryptocurrency2 = CryptocurrencyBuilder().withName(CryptoSymbol.ETHUSDT).withPrice(50000.0).build()
+
+        val cryptos = listOf(cryptocurrency1, cryptocurrency2)
+        `when`(cryptocurrencyRepository.findAll()).thenReturn(cryptos)
+
         mockMvc.perform(
             get("/crypto/quotes")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -44,20 +56,3 @@ class CryptoCurrencyControllerTest {
             .andExpect(status().isBadRequest)
     }
 }
-
-//    @Test
-//    fun `getLast24hsQuotes returns OK status when service returns data`() {
-//        Mockito.`when`(cryptoService.getLast24hsQuotes(CryptoSymbol.BTCUSDT)).thenReturn(listOf<PriceHistory>())
-//        mockMvc.perform(get("/crypto/last24hsQuotes/BTCUSDT")
-//            .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isOk)
-//    }
-//
-//    @Test
-//    fun `getLast24hsQuotes returns BAD_REQUEST status when service throws exception`() {
-//        Mockito.`when`(cryptoService.getLast24hsQuotes(CryptoSymbol.BTCUSDT)).thenThrow(IllegalArgumentException("Error from Service"))
-//        mockMvc.perform(get("/crypto/last24hsQuotes/BTCUSDT")
-//            .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isBadRequest)
-//    }
-//
